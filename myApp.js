e4bd852e40775eb7387712e0c5a3f1c9f3cc51e5
sebastian28em <sebastian28em@gmail.com>
2025-12-10 20:33:26 -0300

    Add files via upload

--- myApp.js
+++ myApp.js
@@ -1,30 +1,16 @@
 let express = require('express');
 let app = express();
 
-// Ejercicio 1: "Hello World" en la consola
 console.log("Hello World");
 
-// Ejercicio 2: servir index.html con ruta absoluta
 app.get("/", function(req, res) {
   const absolutePath = __dirname + "/views/index.html";
   res.sendFile(absolutePath);
 });
 
-// Ruta JSON
-app.get("/json", function(req, res) {
-  res.json({
-    message: "Hello World"
-  });
-});
-
-// PORT dinámico para Render
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, function() {
-  console.log(`Servidor corriendo en el puerto ${PORT}`);
-});
-
 module.exports = app;
+
+
 
 
 
