788e19fcdcd5fd0e8ea6dc07bfbb5d78b0789009
sebastian28em <sebastian28em@gmail.com>
2025-12-10 21:44:35 -0300

    Add files via upload

--- myApp.js
+++ myApp.js
@@ -1,24 +1,34 @@
 let express = require('express');
 let app = express();
-// Ejercicio: 3 servir el archivo index.html en la ruta raíz
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/views/index.html");
-});
-// Ejercicio: 2 servir la cadena en la ruta raíz
-app.get("/", (req, res) => {
+
+// Ejercicio 1: "Hello World" en la consola
+console.log("Hello World");
+
+// Ejercicio 2: servir una cadena en la ruta /hello (para no sobreescribir /)
+app.get("/hello", (req, res) => {
   res.send("Hello Express");
 });
 
-// Ejercicio: 1 "Hello World"
-console.log("Hello World");
+// Ejercicio 3: servir el archivo index.html en la ruta raíz
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
+});
+
+// Ruta JSON
 app.get("/json", (req, res) => {
   res.json({
     message: "Hello World"
   });
 });
 
-module.exports = app;
+// Puerto dinámico para Render
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
 
+// Obligatorio para FCC
+module.exports = app;
 
 
 
