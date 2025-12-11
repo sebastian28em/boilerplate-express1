6bde3c1679330a3c9ef424e187dd5fca622bc07a
sebastian28em <sebastian28em@gmail.com>
2025-12-10 23:44:10 -0300

    Add files via upload

--- myApp.js
+++ myApp.js
@@ -1,29 +1,17 @@
+require('dotenv').config();
 let express = require('express');
 let app = express();
-// Ejercicio 5: servir JSON en /json
-app.get("/json", (req, res) => {
-  res.json({
-    message: "Hello json"
-  });
+
+app.get('/', (req, res) => {
+  res.send('Hello Express');
 });
 
-// Ejercicio: 4 servir activos estáticos desde la carpeta /public
-app.use("/public", express.static(__dirname + "/public"));
-// Ejercicio: 3 servir el archivo index.html en la ruta raíz
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/views/index.html");
-});
-// Ejercicio: 2 servir la cadena en la ruta raíz
-app.get("/", (req, res) => {
-  res.send("Hello Express");
-});
-
-// Ejercicio: 1 "Hello World"
-console.log("Hello World");
-app.get("/json", (req, res) => {
-  res.json({
-    message: "Hello World"
-  });
+app.get('/json', (req, res) => {
+  let message = 'Hello json';
+  if (process.env.MESSAGE_STYLE === 'uppercase') {
+    message = message.toUpperCase();
+  }
+  res.json({ message: message });
 });
 
 module.exports = app;
